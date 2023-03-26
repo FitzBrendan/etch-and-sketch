18d203533a2d9f8c4550c2c11c54f4016aed8542
FitzBrendan <fitzbren@outlook.com>
2023-03-26 13:27:06 -0400

    naming BEM and variables, mouse over to enter, user notifications

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Rev 2.0
 Changed grid of squares created with flex to css grid.
 Bug fix, added user select none to stop mouse dragging.
 Some naming clarifications.
+
+Rev 3.0
+Change html element id's to classes - BEM.
+Add mouseenter event listener to each created padSquare.
+Remove propagated listener on sketchPad(squaresContainer).
+Break out code into individual functions.
+Rename variables for clarification.
+Add Pen/Eraser swatch.
+Add user notifications for selections.
--- script.js
+++ script.js
@@ -1,71 +1,246 @@
 // Etch & Sketch
 
-const sketchPad = document.querySelector(`#sketch_pad__grid--container`);
+// build opening sketch pad with grid lines
+window.addEventListener(`load`, () => {
+  buildSketchPad(quantity);
+  //toggleGridLines(); /////////////////////////////
+});
 
-// get quantity of squares from user, prompt pop window, buildGrid() with input
-const numSquaresBtn = document.querySelector(`#numSquaresPick`);
-numSquaresBtn.addEventListener(
-  "mouseover",
-  () => (numSquaresBtn.style.cursor = `pointer`)
+const sketchPad = document.querySelector(`.f--main__g--sketch_pad`);
+let padSquare;
+let padSquares;
+const initialQuantity = 24
+let quantity = initialQuantity;
+
+let sketchPadColor = `white`;
+const initialPenColor = `black`;
+let penColor = initialPenColor;
+let prevColor = penColor;
+const swatchColor = document.querySelector(`.f--pen_color__color_swatch`);
+const swatchTitle = document.querySelector(`.f--pen_color__pen--content`);
+
+let isButtonDown;
+let isErase = false;
+let isColorOver = false;
+let isGridLines = false;
+
+const btnBlack = document.querySelector(`.f--color_pick__btn--black`);
+btnBlack.addEventListener(`click`, blackPen);
+
+/*
+const btnPickColor = document.querySelector(`.f--color_pick__btn--pick_color`);
+btnPickColor.addEventListener(`click`, pickColor);
+*/
+
+const btnSurprise = document.querySelector(`.f--color_pick__btn--surprise`);
+btnSurprise.addEventListener(`click`, surpriseMe);
+
+/*
+const btnRandom = document.querySelector(`.f--color_pick__btn--random`);
+btnRandom.addEventListener(`click`, randomColor);
+*/
+
+const btnNewSketchPad = document.querySelector(`.f--new_pad__btn--new_sketch-pad`);
+btnNewSketchPad.addEventListener("mouseover", () => {
+  btnNewSketchPad.style.cursor = `pointer`; }
 );
-numSquaresBtn.addEventListener("click", getSquareCount);
-function getSquareCount() {
-  let num = prompt(
-    `Please enter amount of squares for each row, from 1 to 100?`
-  );
-  if (num > 0 && num <= 101) {
-    resetSketchPad();
-    numSquares = parseInt(num);
-    buildGrid(numSquares);
-  } else {
-    alert(`Please enter a number from 1 - 100`);
-  }
-}
+btnNewSketchPad.addEventListener("click", getNewSketchPad);
 
-// reset - remove pad squares in prep to build new sketch pad grid
-function resetSketchPad() {
-  const sketchPad = document.querySelector(`#sketch__pad`);
-  while (sketchPad.firstChild) {
-    sketchPad.removeChild(sketchPad.firstChild);
-  }
-}
+/*
+const btnShading = document.querySelector(`f--new_pad__btn--shading`);
+btnShading.addEventListener(`click`, shading);
+*/
 
-// create grid of squares with css grid
-function buildGrid(quantity) {
+const btnColorOver = document.querySelector(`.f--new_pad__btn--color_over`);
+btnColorOver.addEventListener(`click`, toggleColoringOver);
+
+const btnEraser = document.querySelector(`.f--eraser__btn--eraser`);
+btnEraser.addEventListener(`click`, eraserOn);
+
+const btnGridLines = document.querySelector(`.f--eraser__btn--lines`);
+btnGridLines.addEventListener(`click`, toggleGridLines);
+
+const btnClearGrid = document.querySelector(`.f--eraser__btn--clear`);
+btnClearGrid.addEventListener(`click`, clearSketchPad);
+
+function buildSketchPad(quantity) {
+  resetSketchPad();
+  coloringOn();
+  updateSwatchColor();
   sketchPad.style.gridTemplateColumns = `repeat(${quantity}, 1fr)`;
   sketchPad.style.gridTemplateRows = `repeat(${quantity}, 1fr)`;
   for (let i = 0; i < quantity * quantity; i++) {
-    const padSquare = document.createElement("div");
-    padSquare.classList.add("pad_square");
-    padSquare.addEventListener("mouseover", colorSquare);
-    sketchPad.appendChild(padSquare);
+    const padSquareDiv = document.createElement("div");
+    padSquareDiv.classList.add("pad_square");
+    padSquareDiv.style.backgroundColor = sketchPadColor;
+    sketchPad.appendChild(padSquareDiv);
+    padSquareDiv.addEventListener(`mouseenter`, (e) => {
+      padSquare = e.target;
+      isButtonDown = e.buttons;
+      /////////////////////////////////////////////
+      // padSquareBgColor = e.target.style.backgroundColor;
+      if (isButtonDown) colorPadSquare(penColor);
+    });
+  };
+  padSquares = document.querySelectorAll(`.pad_square`);
+  /////////////////////////////////////////////////////
+  toggleGridLines();
+  
+};
+
+function colorPadSquare(penColor) {
+    if (isColorOver) {
+      colorAnyPadSquare();
+    };
+    if (!isColorOver) {
+      colorEmptyPadSquare();
+    };
+    if (isErase) {
+      colorAnyPadSquare();
+      showErasingPadSquareGridLines();
+    };
+};
+
+function resetSketchPad() {
+  while (sketchPad.firstChild) {
+  sketchPad.removeChild(sketchPad.firstChild);
   }
 }
 
-// colorize square with user selected coloring option
-function colorSquare() {
-  const squaresContainer = document.querySelector(
-    `#sketch_pad__grid--container`
+function blackPen() {
+  penColor = `black`;
+  prevColor = penColor;
+  coloringOn();
+  updateSwatchColor();
+};
+
+function pickColor() {
+
+};
+
+function surpriseMe() {
+  penColor = `green`;
+  prevColor = penColor;
+  coloringOn();
+  updateSwatchColor();
+};
+
+function randomColor() {
+
+};
+
+function swatchTitlePen() {
+  swatchTitle.textContent = `Pen`
+};
+function swatchTitleEraser() {
+  swatchTitle.textContent = `Eraser`
+};
+function updateSwatchColor() {
+  swatchColor.style.backgroundColor = penColor;
+};
+
+function getNewSketchPad() {
+  let num = prompt(
+    `Please enter how many squares you would like, from 2 to 100?`, `24`
   );
-  squaresContainer.addEventListener("mouseover", (e) => {
-    if (e.buttons && e.target.classList.contains(`pad_square`)) {
-      e.target.classList.add("black");
-    }
+  if (num ===  null) return;
+  if (num > 1 && num <= 101) {
+    quantity = parseInt(num);
+    buildSketchPad(quantity);
+    } else {
+    alert(`Please input a number from 2 - 100`);
+    getNewSketchPad();
+  }
+};
+
+function shading() {
+
+};
+
+
+function toggleColoringOver() {
+  isColorOver = !isColorOver;
+  if (isColorOver) {
+    btnColorOver.textContent = `Color Over \u2713`;
+  };
+  if  (!isColorOver) {
+    btnColorOver.textContent = `No Color Over`;
+  };
+};
+function colorAnyPadSquare() {
+  padSquare.style.backgroundColor = penColor;
+  padSquare.classList.remove(`grid_lines`);
+};
+function colorEmptyPadSquare() {
+  let bgColor = padSquare.style.backgroundColor;
+    if (bgColor === sketchPadColor) {
+      colorAnyPadSquare();
+  };
+};
+
+function eraserOn() {
+  isErase = !isErase;
+  if (isErase) {
+    penColor = sketchPadColor;
+    swatchTitleEraser();
+    updateSwatchColor();
+    }else if (!isErase) {
+    coloringOn();
+  }
+};
+
+function coloringOn() {
+  isErase = false;
+  penColor = prevColor;
+  swatchTitlePen();
+  updateSwatchColor();
+};
+
+
+//////////////////////////////////////////////////////
+function toggleGridLines() {
+  isGridLines = !isGridLines;
+  if (isGridLines) {
+    btnGridLines.textContent = `Hide Grid Lines`;
+    showPadSquareGridLines();
+  };
+  if (!isGridLines) {
+    btnGridLines.textContent = `Show Grid Lines`;
+    hidePadSquareGridLines();
+  };
+};
+function showPadSquareGridLines() {
+  padSquares.forEach(square => {
+    ///////////////////////////////////
+    // if (padSquareBgColor === sketchPadColor)
+    let bgColor = square.style.backgroundColor;
+    if (bgColor === sketchPadColor) {
+      square.classList.add(`grid_lines`);
+    };
   });
-}
+};
+function hidePadSquareGridLines() {
+  padSquares.forEach(square => {
+    square.classList.remove(`grid_lines`);
+  });
+};
+function showErasingPadSquareGridLines() {
+  if (isGridLines) padSquare.classList.add(`grid_lines`);
+};
+function hideColoringPadSquareGridLines() {
+  padSquare.classList.remove(`grid_lines`);
+};
 
-// change color for squares coloring per button clicked
-let squareColor = `black`;
-blackBtn = document.querySelector(`#btn__black`);
-blackBtn.addEventListener(`click`, () => (squareColor = `black`));
-whiteBtn = document.querySelector(`#btn__white`);
-whiteBtn.addEventListener(`click`, () => (squareColor = `white`));
-surpriseBtn = document.querySelector(`#btn__surprise`);
-surpriseBtn.addEventListener(`click`, () => (squareColor = `surprise`));
-shadingBtn = document.querySelector(`#btn__shading`);
-shadingBtn.addEventListener(`click`, () => (squareColor = `shade`));
-pickColorBtn = document.querySelector(`#btn__color--pick`);
-pickColorBtn.addEventListener(`click`, () => (squareColor = `color--pick`));
+ 
+function clearSketchPad() {
+  padSquares.forEach(square => {
+    square.style.backgroundColor = sketchPadColor;
+    if (isGridLines) square.classList.add(`grid_lines`);
+  });
+  coloringOn();
+};
 
-// initialize opening grid to 24 x 24
-window.addEventListener(`load`, () => buildGrid(24));
+  
+
+
+
