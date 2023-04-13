4889b5f56cc2b20e191945d7895daffa5751f082
FitzBrendan <fitzbren@outlook.com>
2023-04-13 12:46:37 -0400

    add color picker, eliminate smaller functions, fix button interactions

--- README.md
+++ README.md
@@ -10,9 +10,15 @@ Some naming clarifications.
 
 Rev 3.0
 Change html element id's to classes - BEM.
-Add mouseenter event listener to each created padSquare.
+Add mouseenter event listener to each created enterSketchPadSquare.
 Remove propagated listener on sketchPad(squaresContainer).
 Break out code into individual functions.
 Rename variables for clarification.
 Add Pen/Eraser swatch.
 Add user notifications for selections.
+
+Rev 4.0 Added Color Picker. Moved code blocks from smaller functions directly
+into main functions and into button event handler anonymous functions.
+Abandoned BEM and added id's to html elements. Normalized all colors to RGB format.
+Grouped functions into categories and renamed functions.
+Bug fixes for button interactions.
--- script.js
+++ script.js
@@ -1,246 +1,352 @@
-// Etch & Sketch
+// -- Etch & Sketch -- //
 
-// build opening sketch pad with grid lines
-window.addEventListener(`load`, () => {
-  buildSketchPad(quantity);
-  //toggleGridLines(); /////////////////////////////
-});
+let enterSketchPadSquare;
+let leaveSketchPadSquare;
 
-const sketchPad = document.querySelector(`.f--main__g--sketch_pad`);
-let padSquare;
-let padSquares;
-const initialQuantity = 24
+const initialQuantity = 24;
 let quantity = initialQuantity;
 
-let sketchPadColor = `white`;
-const initialPenColor = `black`;
+const sketchPadColor = `rgb(255, 255, 255)`;
+
+const initialPenColor = `rgb(0, 0, 0)`;
 let penColor = initialPenColor;
-let prevColor = penColor;
-const swatchColor = document.querySelector(`.f--pen_color__color_swatch`);
-const swatchTitle = document.querySelector(`.f--pen_color__pen--content`);
+let prevColor = initialPenColor;
 
-let isButtonDown;
-let isErase = false;
+let isMouseDown = false;
+
+let isSurpriseMe = false;
+let isShading = false;
 let isColorOver = false;
-let isGridLines = false;
+let isErase = false;
+let isGridLines = true;
+let prevGridLines;
 
-const btnBlack = document.querySelector(`.f--color_pick__btn--black`);
-btnBlack.addEventListener(`click`, blackPen);
+//// Buttons - and user selections ////
 
-/*
-const btnPickColor = document.querySelector(`.f--color_pick__btn--pick_color`);
-btnPickColor.addEventListener(`click`, pickColor);
-*/
+const btnBlack = document.querySelector(`#btn--black`);
+btnBlack.addEventListener(`click`, () => {
+  penColor = `rgb(0, 0, 0)`;
+  eraseOff();
+  surprisePenOff();
+});
 
-const btnSurprise = document.querySelector(`.f--color_pick__btn--surprise`);
-btnSurprise.addEventListener(`click`, surpriseMe);
+const btnComputerPick = document.querySelector(`#btn--random`);
+btnComputerPick.addEventListener(`click`, () => {
+  penColor = getRandomColorToRgb();
+  eraseOff();
+  surprisePenOff();
+});
 
-/*
-const btnRandom = document.querySelector(`.f--color_pick__btn--random`);
-btnRandom.addEventListener(`click`, randomColor);
-*/
+const btnSurpriseMe = document.querySelector(`#btn--surprise`);
+btnSurpriseMe.addEventListener(`click`, () => {
+  isSurpriseMe = !isSurpriseMe;
+  if (isSurpriseMe) {
+    btnSurpriseMe.textContent = `Surprise Me \u2713`;
+    eraseOff();
+    shadingOff();
+  }else if (!isSurpriseMe) {
+    btnSurpriseMe.textContent = `Surprise Me`;
+  }; 
+});
 
-const btnNewSketchPad = document.querySelector(`.f--new_pad__btn--new_sketch-pad`);
+const colorPicker = document.querySelector(`#color_picker`);
+colorPicker.addEventListener(`change`, () => {
+  colorPicked = colorPicker.value
+  penColor = hexToRgb(colorPicked);
+  eraseOff();
+  surprisePenOff();
+});
+
+const btnNewSketchPad = document.querySelector(`#btn--new_sketch_pad`);
 btnNewSketchPad.addEventListener("mouseover", () => {
-  btnNewSketchPad.style.cursor = `pointer`; }
-);
+  btnNewSketchPad.style.cursor = `pointer`;
+});
 btnNewSketchPad.addEventListener("click", getNewSketchPad);
 
-/*
-const btnShading = document.querySelector(`f--new_pad__btn--shading`);
-btnShading.addEventListener(`click`, shading);
-*/
+const btnShading = document.querySelector(`#btn--shading`);
+btnShading.addEventListener(`click`, () => {
+    isShading = !isShading;
+    if (isShading) {
+      btnShading.textContent = `Shading \u2713`;
+    }else if (!isShading) {
+    btnShading.textContent = `Shading`;
+    };
+  //};
+  eraseOff();
+  surprisePenOff();
+  colorOverOff();
+});
 
-const btnColorOver = document.querySelector(`.f--new_pad__btn--color_over`);
-btnColorOver.addEventListener(`click`, toggleColoringOver);
-
-const btnEraser = document.querySelector(`.f--eraser__btn--eraser`);
-btnEraser.addEventListener(`click`, eraserOn);
-
-const btnGridLines = document.querySelector(`.f--eraser__btn--lines`);
-btnGridLines.addEventListener(`click`, toggleGridLines);
-
-const btnClearGrid = document.querySelector(`.f--eraser__btn--clear`);
-btnClearGrid.addEventListener(`click`, clearSketchPad);
-
-function buildSketchPad(quantity) {
-  resetSketchPad();
-  coloringOn();
-  updateSwatchColor();
-  sketchPad.style.gridTemplateColumns = `repeat(${quantity}, 1fr)`;
-  sketchPad.style.gridTemplateRows = `repeat(${quantity}, 1fr)`;
-  for (let i = 0; i < quantity * quantity; i++) {
-    const padSquareDiv = document.createElement("div");
-    padSquareDiv.classList.add("pad_square");
-    padSquareDiv.style.backgroundColor = sketchPadColor;
-    sketchPad.appendChild(padSquareDiv);
-    padSquareDiv.addEventListener(`mouseenter`, (e) => {
-      padSquare = e.target;
-      isButtonDown = e.buttons;
-      /////////////////////////////////////////////
-      // padSquareBgColor = e.target.style.backgroundColor;
-      if (isButtonDown) colorPadSquare(penColor);
-    });
-  };
-  padSquares = document.querySelectorAll(`.pad_square`);
-  /////////////////////////////////////////////////////
-  toggleGridLines();
-  
-};
-
-function colorPadSquare(penColor) {
+const btnColorOver = document.querySelector(`#btn--color_over`);
+btnColorOver.addEventListener(`click`, () => {
+    isColorOver = !isColorOver;
     if (isColorOver) {
-      colorAnyPadSquare();
+      btnColorOver.textContent = `Color Over \u2713`;
+    }else if (!isColorOver) {
+    btnColorOver.textContent = `Color Over`;
     };
-    if (!isColorOver) {
-      colorEmptyPadSquare();
-    };
-    if (isErase) {
-      colorAnyPadSquare();
-      showErasingPadSquareGridLines();
-    };
-};
+    eraseOff();
+    shadingOff();
+});
 
-function resetSketchPad() {
-  while (sketchPad.firstChild) {
-  sketchPad.removeChild(sketchPad.firstChild);
-  }
-}
-
-function blackPen() {
-  penColor = `black`;
-  prevColor = penColor;
-  coloringOn();
-  updateSwatchColor();
-};
-
-function pickColor() {
-
-};
-
-function surpriseMe() {
-  penColor = `green`;
-  prevColor = penColor;
-  coloringOn();
-  updateSwatchColor();
-};
-
-function randomColor() {
-
-};
-
-function swatchTitlePen() {
-  swatchTitle.textContent = `Pen`
-};
-function swatchTitleEraser() {
-  swatchTitle.textContent = `Eraser`
-};
-function updateSwatchColor() {
-  swatchColor.style.backgroundColor = penColor;
-};
-
-function getNewSketchPad() {
-  let num = prompt(
-    `Please enter how many squares you would like, from 2 to 100?`, `24`
-  );
-  if (num ===  null) return;
-  if (num > 1 && num <= 101) {
-    quantity = parseInt(num);
-    buildSketchPad(quantity);
-    } else {
-    alert(`Please input a number from 2 - 100`);
-    getNewSketchPad();
-  }
-};
-
-function shading() {
-
-};
-
-
-function toggleColoringOver() {
-  isColorOver = !isColorOver;
-  if (isColorOver) {
-    btnColorOver.textContent = `Color Over \u2713`;
-  };
-  if  (!isColorOver) {
-    btnColorOver.textContent = `No Color Over`;
-  };
-};
-function colorAnyPadSquare() {
-  padSquare.style.backgroundColor = penColor;
-  padSquare.classList.remove(`grid_lines`);
-};
-function colorEmptyPadSquare() {
-  let bgColor = padSquare.style.backgroundColor;
-    if (bgColor === sketchPadColor) {
-      colorAnyPadSquare();
-  };
-};
-
-function eraserOn() {
+const btnEraser = document.querySelector(`#btn--eraser`);
+btnEraser.addEventListener(`click`, () => {
   isErase = !isErase;
   if (isErase) {
-    penColor = sketchPadColor;
-    swatchTitleEraser();
-    updateSwatchColor();
-    }else if (!isErase) {
-    coloringOn();
-  }
-};
+    erasing()
+  }else if (!isErase) {
+   eraseOff();
+  };
+});
 
-function coloringOn() {
-  isErase = false;
-  penColor = prevColor;
-  swatchTitlePen();
-  updateSwatchColor();
-};
-
-
-//////////////////////////////////////////////////////
-function toggleGridLines() {
+const btnGridLines = document.querySelector(`#btn--grid_lines`);
+btnGridLines.addEventListener(`click`, () => {
   isGridLines = !isGridLines;
   if (isGridLines) {
     btnGridLines.textContent = `Hide Grid Lines`;
     showPadSquareGridLines();
-  };
-  if (!isGridLines) {
+  }else if (!isGridLines) {
     btnGridLines.textContent = `Show Grid Lines`;
     hidePadSquareGridLines();
   };
-};
-function showPadSquareGridLines() {
-  padSquares.forEach(square => {
-    ///////////////////////////////////
-    // if (padSquareBgColor === sketchPadColor)
-    let bgColor = square.style.backgroundColor;
-    if (bgColor === sketchPadColor) {
-      square.classList.add(`grid_lines`);
-    };
-  });
-};
-function hidePadSquareGridLines() {
-  padSquares.forEach(square => {
-    square.classList.remove(`grid_lines`);
-  });
-};
-function showErasingPadSquareGridLines() {
-  if (isGridLines) padSquare.classList.add(`grid_lines`);
-};
-function hideColoringPadSquareGridLines() {
-  padSquare.classList.remove(`grid_lines`);
-};
+});
 
- 
-function clearSketchPad() {
-  padSquares.forEach(square => {
+const btnClearSketchPad = document.querySelector(`#btn--clear`);
+btnClearSketchPad.addEventListener(`click`, () => {
+  sketchPadSquares = document.querySelectorAll(`.pad_square`);
+  sketchPadSquares.forEach((square) => {
     square.style.backgroundColor = sketchPadColor;
+    square.style.opacity = 1;
     if (isGridLines) square.classList.add(`grid_lines`);
   });
-  coloringOn();
+  eraseOff();
+});
+//// End -- Buttons - and user selections ////
+
+//// Sketch Pad - build, reset, new ////
+
+function buildSketchPad(quantity) {
+  resetSketchPad();
+  const sketchPad = document.querySelector(`#g__sketch_pad`);
+  sketchPad.style.gridTemplateColumns = `repeat(${quantity}, 1fr)`;
+  sketchPad.style.gridTemplateRows = `repeat(${quantity}, 1fr)`;
+  for (let i = 0; i < quantity * quantity; i++) {
+    const divSketchPadSquare = document.createElement("div");
+    divSketchPadSquare.classList.add("pad_square");
+    divSketchPadSquare.style.backgroundColor = sketchPadColor;
+    sketchPad.appendChild(divSketchPadSquare);
+    divSketchPadSquare.addEventListener(`mouseenter`, (e) => {
+      e.stopPropagation();
+      enterSketchPadSquare = e.currentTarget;
+      isMouseDown = e.buttons;
+      enteringSketchPadSquare();
+    });
+    divSketchPadSquare.addEventListener(`mouseleave`, (e) => {
+      e.stopPropagation();
+      leaveSketchPadSquare = e.currentTarget;
+      leavingSketchPadSquare();
+    });
+  }
+  showPadSquareGridLines();
+  updateSwatch();
+}
+
+function resetSketchPad() {
+  const sketchPad = document.querySelector(`#g__sketch_pad`);
+  while (sketchPad.firstChild) {
+    sketchPad.removeChild(sketchPad.firstChild);
+  };
+}
+
+function getNewSketchPad() {
+  eraseOff();
+  let num = prompt(
+    `Please enter how many squares you would like,
+    from 2 to 100?`,
+    `24`
+  );
+  if (num === null) return;
+  if (num > 1 && num <= 101) {
+    quantity = parseInt(num);
+    buildSketchPad(quantity);
+  } else {
+    alert(`Please input a number from 2 - 100`);
+    getNewSketchPad();
+  };
+}
+//// End -- Sketch Pad - build, reset, new ////
+
+//// Sketch Pad Square Listeners - enter, leave ////
+
+function enteringSketchPadSquare() {
+  prevColor = enterSketchPadSquare.style.backgroundColor;
+  prevGridLines = enterSketchPadSquare.style.border;
+  if (isSurpriseMe) {
+    penColor = getRandomColorToRgb();
+    updateSwatch();
+  }
+  if (!isMouseDown) {
+    enterSketchPadSquare.style.borderRadius = `50%`;
+    if (!isErase) {
+      enterSketchPadSquare.style.backgroundColor = penColor;
+    }else if (isErase) {
+    enterSketchPadSquare.style.backgroundColor = sketchPadColor;
+    };
+  }else if (isMouseDown) {
+    if (isColorOver) {
+      coloringOverSquare();
+    }else if (!isColorOver) {
+      coloringEmptySquare();
+    };
+    if (isShading) shading();
+    if (isErase) erasing();
+  };
+}
+
+function leavingSketchPadSquare() {
+  leaveSketchPadSquare.style.borderRadius = `0%`;
+  if (!isMouseDown) {
+    getPrevValuesSquareLeaving();
+  };
+}
+//// End -- Sketch Pad Square Listeners - enter, leave ////
+
+//// Coloring ////
+
+function shading() {
+  let bgColor = enterSketchPadSquare.style.backgroundColor;
+  let bgOpacity = 
+    parseFloat(
+    window.getComputedStyle(enterSketchPadSquare)
+    .getPropertyValue(`opacity`)
+  );
+  if (prevColor == sketchPadColor) {
+    enterSketchPadSquare.style.opacity = 0.1;
+  }else if (prevColor == penColor && bgOpacity < 1) {
+    bgOpacity += 0.1;
+      enterSketchPadSquare.style.opacity = bgOpacity;
+  };
+}
+
+function coloringOverSquare() {
+  enterSketchPadSquare.style.backgroundColor = penColor;
+  enterSketchPadSquare.style.opacity = 1;
+  if (isMouseDown) removePadSquareGridLines();
+}
+function coloringEmptySquare() {
+let bgColor = enterSketchPadSquare.style.backgroundColor;
+if (bgColor === sketchPadColor) {
+  coloringOverSquare();
 };
+}
 
-  
+function getPrevValuesSquareLeaving() {
+  leaveSketchPadSquare.style.backgroundColor = prevColor;
+  leaveSketchPadSquare.style.border = prevGridLines;
+}
+//// End -- Coloring ////
 
+//// Erasing ////
 
+function erasing() {
+  btnEraser.textContent = `Eraser \u2713`;
+  enterSketchPadSquare.style.backgroundColor = sketchPadColor;
+  enterSketchPadSquare.style.opacity = 1;
+  updateSwatch();
+  if (isGridLines) addBackPadSquareGridLines();
+}
+//// End -- Erasing ////
 
+//// Turn Off Features - surprise, shading, color over, erase ////
+
+function surprisePenOff() {
+  isSurpriseMe = false;
+  btnSurpriseMe.textContent = `Surprise Me`
+}
+function shadingOff() {
+  isShading = false;
+  btnShading.textContent = `Shading`;
+}
+function colorOverOff() {
+  isColorOver = false;
+  btnColorOver.textContent = `Color Over`;
+}
+
+function eraseOff() {
+  isErase = false;
+  btnEraser.textContent = `Eraser`;
+  updateSwatch();
+}
+//// End -- Turn Off Features ////
+
+//// Grid Lines ////
+
+function showPadSquareGridLines() {
+  sketchPadSquares = document.querySelectorAll(`.pad_square`);
+  sketchPadSquares.forEach((padSquare) => {
+    let bgColor = padSquare.style.backgroundColor;
+    if (bgColor === sketchPadColor) {
+      padSquare.classList.add(`grid_lines`);
+    }
+  });
+}
+function hidePadSquareGridLines() {
+  sketchPadSquares = document.querySelectorAll(`.pad_square`);
+  sketchPadSquares.forEach((padSquare) => {
+    padSquare.classList.remove(`grid_lines`);
+  });
+}
+function addBackPadSquareGridLines() {
+  enterSketchPadSquare.classList.add(`grid_lines`);
+}
+function removePadSquareGridLines() {
+  enterSketchPadSquare.classList.remove(`grid_lines`);
+}
+//// End -- Grid Lines ////
+
+//// Pen & Swatch ////
+
+function updateSwatch() {
+  const swatchTitle = document.querySelector(`#pen--title`);
+  const swatchColor = document.querySelector(`#swatch--color`);
+  if (isErase) {
+    swatchTitle.textContent = `Eraser`;
+    swatchColor.style.backgroundColor = sketchPadColor;
+  }else if (!isErase) {
+    swatchTitle.textContent = `Pen`;
+    swatchColor.style.backgroundColor = penColor;
+  };
+}
+//// End -- Pen & Swatch ////
+
+//// Generic and Page Load ////
+
+function getRandomColorToRgb() {
+  let randomColor = `#${Math.floor(
+    Math.random()*16777215).toString(16)}`;
+  if (randomColor === sketchPadColor) {
+    getRandomColorToRgb();
+  };
+  return hexToRgb(randomColor);
+}
+
+function hexToRgb(hex) {
+  const r = parseInt(hex.substring(1, 3), 16);
+  const g = parseInt(hex.substring(3, 5), 16);
+  const b = parseInt(hex.substring(5, 7), 16);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function rgbToHex(rgb) {
+  let rgbValues = rgb.match(/(\d+),\s(\d+),\s(\d+)/);
+  var hexValues = [
+    parseInt(rgbValues[1]).toString(16).padStart(2, '0').toUpperCase(),
+    parseInt(rgbValues[2]).toString(16).padStart(2, '0').toUpperCase(),
+    parseInt(rgbValues[3]).toString(16).padStart(2, '0').toUpperCase()
+  ];
+  return `#${hexValues.join("")}`;
+}
+
+window.addEventListener(`load`, () => {
+  buildSketchPad(quantity);
+});
